f083ddbbca988e7e707643fb290d7a499342cf8c
trapple <trapplejp@gmail.com>
2014-10-17 14:14:29 +0900

    grunt/* は別リポジトリにする

--- grunt/connect.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  livereload: {
-    options: {
-      base: "./app/"
-    }
-  }
-};
--- grunt/watch.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  dev: {
-    files: ["src/**/*.js", "src/tmpl/*.html", "spec/**/*.js", "Gruntfile.js", "package.json", "grunt/*"],
-    tasks: ["develop"]
-  },
-  options: {
-    livereload: true
-  }
-};
